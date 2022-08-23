5d6d05aeac50b8719d52e259edf83a3f0fe34fe3
Eric Seppanen <eds@reric.net>
2022-08-22 20:07:29 -0700

    add more examples
    Use cargo sync-readme to update the README.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,13 @@
 //! ```
 //! use escape8259::{escape, unescape};
 //!
+//! let s = "A null (\0) and a double-quote (\")";
+//! assert_eq!(escape(s), r#"A null (\u0000) and a double-quote (\")"#);
+//!
+//! let crab = r#"This is a crab: \ud83e\udd80"#;
+//! assert_eq!(unescape(crab).unwrap(), "This is a crab: 🦀");
+//!
+//! // We accept encodings that weren't really necessary.
 //! assert_eq!(unescape(r#"\u0041\n"#).unwrap(), "A\n");
 //!
 //! let multiline = r#"hello
